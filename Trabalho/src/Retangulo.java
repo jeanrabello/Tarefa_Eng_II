7946fc1e7446c5bccad67616f05034c3a9e20848
unknown <gustavodosanjosfernandes@gmail.com>
2021-05-05 18:32:14 -0300

    Adicionada classe Retângulo

---
+++ Trabalho/src/Retangulo.java
@@ -0,0 +1,16 @@
+
+public class Retangulo {
+	public double areaRetangulo(double base, double altura) {
+		double area;
+		
+		area = base *  altura;
+		return area;
+	}
+	
+	public double perimetroRetangulo(double base, double altura) {
+		double perimetro;
+		
+		perimetro = 2*(base + altura);
+		return perimetro;
+	}
+}
