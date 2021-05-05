77ba316a75d326b9eb817118061065af3d6d31fd
jeanrabello <jean.rabello.prog@gmail.com>
2021-05-05 17:47:29 -0300

    Adicionada classe Main

---
+++ Trabalho/src/Main.java
@@ -0,0 +1,62 @@
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	static Circunferência c = new Circunferência();
+	static Retângulo r = new Retângulo();
+	static boolean wl = true;
+	
+	public static void main(String[] args) {
+		int decisao;
+		double raio;
+		double base;
+		double altura;
+		
+	while(wl){	
+		decisao = Integer.parseInt(JOptionPane.showInputDialog("Calcular: \n\n1 - Circunferência \n2 - Retângulo \n3 - Sair"));
+		switch(decisao) {
+			case 1:
+				decisao = Integer.parseInt(JOptionPane.showInputDialog("Calcular: \n\n1 - Área \n2 - Perímetro"));
+				switch(decisao) {
+					case 1:
+						raio = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor do raio"));
+						JOptionPane.showMessageDialog(null, "Área da Circunferência: " + c.areaCircunferencia(raio));
+						break;
+					case 2:
+						raio = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor do raio"));
+						JOptionPane.showMessageDialog(null, "Perímetro da Circunferência: " + c.areaCircunferencia(raio));
+						break;
+					default:
+						JOptionPane.showMessageDialog(null, "Escolha inválida");
+						break;
+				}
+				break;
+			case 2:
+				decisao = Integer.parseInt(JOptionPane.showInputDialog("Calcular: \n\n1 - Área \n2 - Perímetro"));
+				switch(decisao) {
+					case 1:
+						base = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor da base"));
+						altura = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor da altura"));
+						JOptionPane.showMessageDialog(null, "Área da Retângulo: " + r.areaRetangulo(base, altura));
+						break;
+					case 2:
+						base = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor da base"));
+						altura = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor da altura"));
+						JOptionPane.showMessageDialog(null, "Perímetro da Retângulo: " + r.perimetroRetangulo(base, altura));
+						break;
+					default:
+						JOptionPane.showMessageDialog(null, "Escolha inválida");
+				}
+				break;
+			case 3:
+				wl = false;
+				break;
+			default:
+				JOptionPane.showMessageDialog(null, "Escolha inválida");
+				break;
+			}
+		}
+	}
+
+}
